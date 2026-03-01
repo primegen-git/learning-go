911c227dda7564f8523eae505e5cbb9530514a25
Ujjawal Kumar <ujjawal@luganodes.com>
2026-03-01 07:30:21 +0530

    feat: append method

--- slices/main.go
+++ slices/main.go
@@ -15,4 +15,25 @@ func main() {
 	// len built-in command
 	fmt.Println("Length of x: ", len(x))
 	fmt.Println("Length of y: ", len(y))
+
+	// append to new array
+	var nx []int
+
+	nx = append(nx, 10)
+
+	fmt.Println("nx: ", nx)
+
+	// append to existing array
+	var ny = []int{10, 20, 30}
+
+	ny = append(ny, 40, 50)
+
+	fmt.Println("ny: ", ny)
+
+	// append to an existing slices
+
+	var nz = []int{5}
+
+	nz = append(nz, ny...) // ... is like expanding the ny as append(nz, 10, 20, 30, 40, 50)
+
 }
