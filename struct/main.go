3dc893f28665356ea523304b5b00b406bdb13e25
Ujjawal Kumar <ujjawal@luganodes.com>
2026-03-02 03:49:03 +0530

    feat: declare and assign struct

---
+++ struct/main.go
@@ -0,0 +1,30 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Leanring struct")
+
+	// declaring a struct
+	type person struct {
+		name string
+		age  int
+	}
+
+	// creating variable for struct
+
+	var sasuke person
+	fmt.Println("sasuke: ", sasuke)
+
+	// assigning values without using field of struct
+
+	var naruto person = person{"uzumaki naruto", 10}
+	fmt.Println("naruto: ", naruto)
+
+	// assigning with using the field of struct
+	var rock_lee person = person{
+		name: "rock_lee",
+		age:  30,
+	}
+	fmt.Println("rock_lee: ", rock_lee)
+}
