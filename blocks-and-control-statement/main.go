3d9f9eed406a265e1a7ce4e142b76fb9afead044
Ujjawal Kumar <ujjawal@luganodes.com>
2026-03-03 12:05:21 +0530

    feat: shadowing

--- blocks-and-control-statement/main.go
+++ blocks-and-control-statement/main.go
@@ -4,4 +4,21 @@ import "fmt"
 
 func main() {
 	fmt.Println("Learning blocks and contorl statement")
+
+	// understanding scoping
+
+	x := 10
+
+	if x > 5 {
+
+		fmt.Println("start of if block")
+		fmt.Println("x: ", x)
+
+		fmt.Println("redeclared x")
+		x := 5
+		fmt.Println("x: ", x)
+		fmt.Println("end of if block")
+	}
+
+	fmt.Println("x: ", x)
 }
